4a35049865f889fd2abc49e2908dd4f024dd65e8
jacobfarmer86 <jacobfarmer_2021@depauw.edu>
2019-10-08 13:53:28 -0400

    hghg

---
+++ Assignments/2019-10-08_Lab6.R
@@ -0,0 +1,31 @@
+# Clean up the working environment
+rm(list = ls())
+# Verify working directory, should be ~/Documents/Analyses/lastname_first
+getwd()
+
+# Load tidyverse
+library("tidyverse")
+# Check for updates
+tidyverse_update()
+
+# To perform sign tests, install and load the package DescTools
+# install.packages("DescTools")
+library("DescTools")
+
+# To simplify summary statistics, install and load the package summarytools
+install.packages("summarytools")
+library("summarytools")
+
+#Chapter 13
+#Question 20
+#a. 2-sample t-test, 
+#b. 
+
+
+
+#Question 25
+
+
+
+
+#Question 26
